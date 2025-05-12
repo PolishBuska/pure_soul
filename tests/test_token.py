c728947e023819fd4d2330fb1903e98f76e6e8c9
S1m6ad <symanby2002@gmail.com>
2025-05-12 06:52:36 +0300

    Add files via upload

--- tests/test_token.py
+++ tests/test_token.py
@@ -1,12 +1,8 @@
 import pytest
-from jose.jwt import decode
-from datetime import datetime, timedelta, timezone
+import jwt
+from datetime import datetime, timedelta
 from unittest.mock import Mock
-from src.adapters.token_generator import JoseTokenGenerator
-"""
-    ты импортировал протокол а не имплементацию
-    from src.adapters.token_generator import TokenGenerator - так не надо
-"""
+from src.adapters.token_generator import TokenGenerator
 
 @pytest.fixture
 def mock_user():
@@ -14,41 +10,27 @@ def mock_user():
     user = Mock()
     user.id = 123
     user.grants = ['read', 'write']
-    # Здесь в целом норм уже есть гранты они лежат в домене
     user.is_adult = True
     user.subscription_id = 'sub123'
     user.username = 'testuser'
     user.email = 'testuser@example.com'
-    """
-        Не нужно использовать Мок
-        есть же модель
-        from src.domain.iam.user import BaseUser
-        user = BaseUser(
-            поле = значение
-        )
-        
-    """
     return user
 
 def test_generate_tokens(mock_user):
     """Тестируем метод generate_tokens"""
 
     secret = "supersecretkey"
-    #Кирилл надо было взять секрет из мейна ну ладно
-    token_generator = JoseTokenGenerator(secret)
+    token_generator = TokenGenerator(secret)
 
-    token = token_generator.generate_tokens(mock_user)
-    # надо смотреть что обьект/метод возвращает после вызова там нет двух токенов там класс токен в котором два токена
-
+    access_token, refresh_token = token_generator.generate_tokens(mock_user)
 
     # Проверяем, что токены были созданы
-    assert token.access_token is not None
-    assert token.refresh_token is not None
+    assert access_token is not None
+    assert refresh_token is not None
 
     # Декодируем токены и проверяем их содержимое
-    # Смотри какой библиотекой я пользовался для токенов там не jwt там jose
-    decoded_access_token = decode(token.access_token, secret, algorithms=['HS256'])
-    decoded_refresh_token = decode(token.refresh_token, secret, algorithms=['HS256'])
+    decoded_access_token = jwt.decode(access_token, secret, algorithms=['HS256'])
+    decoded_refresh_token = jwt.decode(refresh_token, secret, algorithms=['HS256'])
 
     # Проверяем, что данные в access токене верные
     assert decoded_access_token['user_id'] == mock_user.id
@@ -69,8 +51,8 @@ def test_generate_tokens(mock_user):
     assert 'jti' in decoded_refresh_token
 
     # Проверяем, что срок действия (exp) для access токена через 15 минут
-    exp_access = datetime.fromtimestamp(decoded_access_token['exp'], tz=timezone.utc)
-    assert exp_access - datetime.now(timezone.utc) < timedelta(minutes=16)
+    exp_access = datetime.fromtimestamp(decoded_access_token['exp'])
+    assert exp_access - datetime.now() < timedelta(minutes=16)
 
     # Проверяем, что срок действия (exp) для refresh токена через 7 дней
     exp_refresh = datetime.fromtimestamp(decoded_refresh_token['exp'])
