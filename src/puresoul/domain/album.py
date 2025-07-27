be8daf892726a96324a1167bb0bc0940a5b0f898
stanislavmisko <miskostanislau@gmail.com>
2025-07-27 23:17:35 +0300

    Added a rich domain model Album. Created a domain function publish_album. covered exception handling and docstrings for this model

--- src/puresoul/domain/album.py
+++ src/puresoul/domain/album.py
@@ -3,7 +3,9 @@ from datetime import datetime
 from typing import List, Optional
 
 from .artist import Artist
+from .exceptions import NotAuthorizedException, TooFewItemsException, AlreadyPublic
 from .genre import Genre
+from .iam.user import BaseUser
 from .types import (
     AlbumId,
     AlbumDescription,
@@ -23,3 +25,29 @@ class Album:
     updated_at: Optional[datetime]
     is_released: bool
     author_id: int
+
+    def publish_album(
+            self,
+            cu: BaseUser
+    ) -> None:
+        """
+            This function will publish the requested album
+            This function doesn't return anything
+            It will publish the requested album by changing its internal state
+
+            :param cu: The one whose requesting a publishing
+            :return: None
+        """
+        if self.author_id != cu.id:
+            raise NotAuthorizedException(
+                "cannot access this feature"
+            )
+        if len(self.songs) <= 3:
+            raise TooFewItemsException(
+                "too few songs to publish, please populate the album"
+            )
+        if self.is_released:
+            raise AlreadyPublic(
+                "this album is already released"
+            )
+        self.is_released = True
